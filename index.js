fc6102d481b7c9a5beb11cc3d3485c0207856135
naima-tushin <naima39tushin@gmail.com>
2024-05-14 15:32:47 +0600

    Fetched Data For Food Details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,6 +32,7 @@ async function run() {
         // ? Collection ID
         const database = client.db("harvestFood");
         const foodCollection = database.collection("foods");
+        const requestFoodCollection = database.collection("requestFood");
          
 
         app.get('/allFood', async (req, res) => {
@@ -41,10 +42,65 @@ async function run() {
 
         });
 
+        app.get('/foodDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.get('/myFood/:donatorEmail', async (req, res) => {
+            const donatorEmail = req.params.donatorEmail;
+            const query = { donatorEmail: donatorEmail }; // Assuming 'userEmail' is the field name
+            const cursor = foodCollection.find(query);
+            const results = await cursor.toArray();
+            res.send(results);
+        });
+
+        app.get('/myRequestFood/:userEmail', async (req, res) => {
+            const userEmail = req.params.userEmail;
+            const query = { userEmail: userEmail }; 
+            const cursor = requestFoodCollection.find(query);
+            const results = await cursor.toArray();
+            res.send(results);
+        });
+
+        app.put('/foodUpdate/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id)}
+            const options = { upsert: true };
+            const updatedFood = req.body;
+            const food = {
+                $set: {
+                    foodName: updatedFood.foodName, 
+                    foodImage: updatedFood.foodImage, 
+                    foodStatus: updatedFood.foodStatus, 
+                    foodQuantity: updatedFood.foodQuantity, 
+                    pickupLocation: updatedFood.pickupLocation, 
+                    expiredDateTime: updatedFood.expiredDateTime, 
+                }
+            }
+            const result = await foodCollection.updateOne(filter, food, options);
+            res.send(result);
+        });
+
         app.post('/addFood', async (req, res) => {
             const food = req.body;
             console.log('new food', food);
             const result = await foodCollection.insertOne(food);
+            res.send(result);
+        });
+        app.post('/addRequestFood', async (req, res) => {
+            const food = req.body;
+            console.log('new request food', food);
+            const result = await requestFoodCollection.insertOne(food);
+            res.send(result);
+        });
+
+        app.delete('/foodDelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await foodCollection.deleteOne(query);
             res.send(result);
         });
 
